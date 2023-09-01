70113113cd8c4b0c584bf14c7b09921745625660
Jesse Silva <jesse.nunes@mail.uft.edu.br>
2023-09-01 16:14:39 -0300

    medias

--- main.c
+++ main.c
@@ -1,12 +1,85 @@
-/**************************************************************************************
-* PROG:             PE Implementaçao                                                  *
-* AUTOR:            Jesse Jilva                                                       *
-* DATA:             01/09/2023                                                        *
-* ATUALIZADO EM:    00/00/2023  00:00                                                 *
-**************************************************************************************/
+/***************************************************************************************
+* PROG:             PE Implementaçao                                                   *
+* AUTOR:            Jesse Jilva                                                        *
+* DATA:             01/09/2023                                                         *
+* ATUALIZADO EM:    00/00/2023  00:00                                                  *
+***************************************************************************************/
 #include "operacoes.h"
+
+/********************************** MEDIA ARITMETICA **********************************/
+void media_nao_agrupada(){
+    system("cls");
+    float *elementos;
+    int n;
+    
+    printf("\tMedia nao Agrupada\n\n");
+    n=PriNum("Escreva o numero de elementos: ");
+    elementos = (float *)malloc(n*sizeof(float)); // determina o tam do vetor 
+    putchar('\n');
+
+    for(int i=0;i<n;i++){
+        elementos[i]=PriNumf("Escreva o elemento: ");
+        
+    }
+    printf("\nMedia = %.2f",media(n,elementos));
+}
+
+void media_agrupada(){
+    system("cls");
+    float *elementos,*frequencia,aux1,aux2;
+    int n,op;
+    
+    printf("\tMedia Agrupada\n\n");
+    n=PriNum("Escreva o numero de elementos: ");
+    elementos = (float *)malloc(n*sizeof(float)); // determina o tam do vetor 
+    frequencia = (float *)malloc(n*sizeof(float)); // determina o tam do vetor 
+    putchar('\n');
+
+    do{
+        printf("\tTem Amplitude?\n\n");
+        printf("1- Sim\n2- Nao\n");
+        op=PriNum("Escolha uma das opcoes acima: ");
+    }while(op!=1&&op!=2);
+    if(op==1){
+        for(int i=0;i<n;i++){
+        aux1=PriNumf("De: ");
+        aux2=PriNumf("Ate: ");
+        elementos[i]=(aux2+aux1)/2;
+        frequencia[i]=PriNumf("Escreva a frequencia: ");
+        }  
+    }else{
+        for(int i=0;i<n;i++){
+        elementos[i]=PriNumf("Escreva o elemento: ");
+        frequencia[i]=PriNumf("Escreva a frequencia: ");
+        }  
+    }
+
+
+    printf("\nMedia = %.2f",media_agr(n,elementos,frequencia));
+}
 
 int main(){
 
-    printf("\tImplementacao da materia de Probabilidade Estatistica\n");
+    int op=0;
+    do{
+        system("cls");
+        printf("\tImplementacao da materia de Probabilidade Estatistica\n\n");
+        printf("1- Media\n2- Mediana\n3- Moda\n");
+        op=PriNum("Escolha uma das opcoes acima: ");
+    }while(op!=1&&op!=2&&op!=3);
+    if(op==1){
+        do{
+        system("cls");
+        printf("\tCalculo de Media\n\n");
+        printf("1- Nao Agrupada\n2- Agrupada\n");
+        op=PriNum("Escolha uma das opcoes acima: ");
+        }while(op!=1&&op!=2);
+        if(op==1){
+            media_nao_agrupada();
+        }else{
+            media_agrupada();
+        }
+    }
+    
+    
 }
--- operacoes.h
+++ operacoes.h
@@ -13,4 +13,34 @@ int PriNum(char* s){
     return num;
 }
 
+int PriNumf(char* s){
+    float num;
+
+    printf("%s",s);
+    scanf("%f",&num);
+
+    return num;
+}
+
+float media(int N, float *Xi){
+    float somatorio=0,U=0;
+    for(int i=0;i<N;i++){
+        somatorio+=(Xi[i]);
+    }
+    U=somatorio/N;
+    return U;
+}
+
+float media_agr(int N, float *Xi,float *Fi){
+    float somatorio=0,U=0,F=0;
+    for(int i=0;i<N;i++){
+        somatorio+=(Xi[i]*Fi[i]);
+    }
+    for(int i=0;i<N;i++){
+        F+=Fi[i];
+    }
+    U=somatorio/F;
+    return U;
+}
+
 #endif
