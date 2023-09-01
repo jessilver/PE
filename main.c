c9615870934c510ecec3b10baa2fbdd0a4c9bf3e
Jesse Silva <jesse.nunes@mail.uft.edu.br>
2023-09-01 13:46:38 -0300

    bibliioteca

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
-#include <stdio.h>
+/**************************************************************************************
+* PROG:             PE Implementaçao                                                  *
+* AUTOR:            Jesse Jilva                                                       *
+* DATA:             01/09/2023                                                        *
+* ATUALIZADO EM:    00/00/2023  00:00                                                 *
+**************************************************************************************/
+#include "operacoes.h"
 
 int main(){
-    printf("hello world");
-    return 0;
+
+    printf("\tImplementacao da materia de Probabilidade Estatistica\n");
 }
---
+++ operacoes.h
@@ -0,0 +1,16 @@
+#ifndef OPERACOES_H
+#define OPERACOES_H
+#include<math.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+int PriNum(char* s){
+    int num;
+
+    printf("%s",s);
+    scanf("%d",&num);
+
+    return num;
+}
+
+#endif
